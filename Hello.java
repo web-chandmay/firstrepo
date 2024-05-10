e932b423263062175105f1a816b45caf47a110c8
sekharchandadhikari <136904419+sekharchandadhikari@users.noreply.github.com>
2024-05-10 21:58:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, Chandra Sekhar Adhikari!");
+        System.out.println("Hello, WEBChand How r u!");
     }
 }
